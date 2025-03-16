3c752c61fcbdb2eaa0c438d887336994ef56fe82
Neha-Dube <103204443+Neha-Dube@users.noreply.github.com>
2025-03-16 16:52:53 +0000

    datatype in JS

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict;"  // writing this at the start of the file means treat all JS code as newer version
+
+/* alert(3+5) won't creat alert saying 8 as we are using nodejs not browser
+alert("hello")
+*/
+
+//Original documentation for JS is ECMAScript(https://tc39.es/ecma262/#sec-intro)not mdn, mdn us good but privided by mozilla org
+
+/*primitive dt */
+//number
+//bigint
+//string
+//boolean
+//null==> it represents an empty value
+//undefined
+//symbol
+
+console.log(typeof undefined) //undefined
+console.log(typeof null)  //object
+
+//object
+// 
